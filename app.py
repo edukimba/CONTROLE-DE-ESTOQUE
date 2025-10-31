efca50214d28c895d54f2495f4696968c358124d
Eduardo Kimba <eduardo3287.santos@gmail.com>
2025-10-30 23:36:33 -0300

    routes pt 5

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from database import db
+from routes.routes_produtos import app_routes as produtos_routes
+from routes.routes_movimentacoes import app_routes as movimentacoes_routes
+
+app = Flask(__name__)
+
+#CONFIGURAR BANCO DE DADOS:p
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estoque.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#INICIAR O BANCO DE DADOS:
+
+db.init_app(app)
+
+#REGISTRAR BLUEPRINTS:
+
+app.register_blueprint(produtos_routes, url_prefix='/produtos')
+app.register_blueprint(movimentacoes_routes, url_prefix='/movimentacoes')
+
+@app.route('/')
+def index():
+    return "API DE ESTOQUE ONLINE!"
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
